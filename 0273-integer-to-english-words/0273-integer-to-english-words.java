939880f010fac59aa8c9dbfa547e7249d171a5b5
Priyanshu Singh <120112862+PriyanshuSingh06@users.noreply.github.com>
2026-07-24 21:17:54 +0530

    Time: 6 ms (64.53%), Space: 44.2 MB (65.76%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,11 +1,11 @@
 class Solution {
 
     private final String[] below20 = {
-        "", "One", "Two", "Three", "Four", "Five",
-        "Six", "Seven", "Eight", "Nine", "Ten",
-        "Eleven", "Twelve", "Thirteen", "Fourteen",
-        "Fifteen", "Sixteen", "Seventeen", "Eighteen",
-        "Nineteen"
+        "", "One", "Two", "Three", "Four",
+        "Five", "Six", "Seven", "Eight", "Nine",
+        "Ten", "Eleven", "Twelve", "Thirteen",
+        "Fourteen", "Fifteen", "Sixteen",
+        "Seventeen", "Eighteen", "Nineteen"
     };
 
     private final String[] tens = {
@@ -13,36 +13,46 @@ class Solution {
         "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
     };
 
+    private final String[] units = {
+        "", "Thousand", "Million", "Billion"
+    };
+
     public String numberToWords(int num) {
+
         if (num == 0) {
             return "Zero";
         }
 
-        if (num >= 1_000_000_000) {
-            int rem = num % 1_000_000_000;
+        String result = "";
+        int index = 0;
 
-            return helper(num / 1_000_000_000) + " Billion" +
-                   (rem != 0 ? " " + numberToWords(rem) : "");
+        while (num > 0) {
+
+            int chunk = num % 1000;
+
+            if (chunk != 0) {
+                String part = convert(chunk);
+
+                if (!units[index].isEmpty()) {
+                    part += " " + units[index];
+                }
+
+                if (result.isEmpty()) {
+                    result = part;
+                } else {
+                    result = part + " " + result;
+                }
+            }
+
+            num /= 1000;
+            index++;
         }
 
-        if (num >= 1_000_000) {
-            int rem = num % 1_000_000;
-
-            return helper(num / 1_000_000) + " Million" +
-                   (rem != 0 ? " " + numberToWords(rem) : "");
-        }
-
-        if (num >= 1_000) {
-            int rem = num % 1_000;
-
-            return helper(num / 1_000) + " Thousand" +
-                   (rem != 0 ? " " + numberToWords(rem) : "");
-        }
-
-        return helper(num);
+        return result;
     }
 
-    private String helper(int num) {
+    private String convert(int num) {
+
         if (num == 0) {
             return "";
         }
@@ -53,14 +63,14 @@ class Solution {
 
         if (num < 100) {
             return tens[num / 10] +
-                   (num % 10 != 0
-                       ? " " + below20[num % 10]
-                       : "");
+                   (num % 10 == 0
+                       ? ""
+                       : " " + below20[num % 10]);
         }
 
         return below20[num / 100] + " Hundred" +
-               (num % 100 != 0
-                   ? " " + helper(num % 100)
-                   : "");
+               (num % 100 == 0
+                   ? ""
+                   : " " + convert(num % 100));
     }
 }
