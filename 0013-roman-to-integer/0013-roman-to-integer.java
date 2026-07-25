04882d9c8736aa9fa3ec2fc2c26ce4d22c7a96cf
Priyanshu Singh <120112862+PriyanshuSingh06@users.noreply.github.com>
2026-07-25 18:00:37 +0530

    Time: 3 ms (78.21%), Space: 46.3 MB (75.57%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,37 +1,39 @@
 class Solution {
-    public int romanToInt(String s) {
+   public int romanToInt(String s) {
+        return helper(s, 0);
+    }
 
-        HashMap<Character, Integer> map = new HashMap<>();
+    private int helper(String s, int i) {
+        if (i >= s.length()) {
+            return 0;
+        }
 
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        int curr = romanValue(s.charAt(i));
 
-        int sum = 0;
+        // Check if there is a next character
+        if (i + 1 < s.length()) {
+            int next = romanValue(s.charAt(i + 1));
 
-        for (int i = 0; i < s.length(); i++) {
-
-            int current = map.get(s.charAt(i));
-
-            if (i < s.length() - 1) {
-
-                int next = map.get(s.charAt(i + 1));
-
-                if (current < next) {
-                    sum -= current;
-                } else {
-                    sum += current;
-                }
-
-            } else {
-                sum += current;
+            // Subtractive case (IV, IX, XL, XC, CD, CM)
+            if (curr < next) {
+                return next - curr + helper(s, i + 2);
             }
         }
 
-        return sum;
+        // Normal case
+        return curr + helper(s, i + 1);
+    }
+
+    private int romanValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
