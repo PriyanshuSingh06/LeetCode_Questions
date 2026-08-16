18a6cc22d2b95157b6b8f804415a4cef1debf7e8
Priyanshu Singh <120112862+PriyanshuSingh06@users.noreply.github.com>
2026-08-16 22:29:15 +0530

    Time: 5 ms (80.98%), Space: 46.3 MB (80.17%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        // Base index
+        stack.push(-1);
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+
+                stack.push(i);
+
+            } else {
+
+                stack.pop();
+
+                if (stack.isEmpty()) {
+
+                    // Current ')' cannot be matched
+                    stack.push(i);
+
+                } else {
+
+                    int length = i - stack.peek();
+
+                    maxLength = Math.max(maxLength, length);
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
