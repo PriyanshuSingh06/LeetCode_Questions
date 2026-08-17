8b3b5df9924464886824443a0f52139823760bcf
Priyanshu Singh <120112862+PriyanshuSingh06@users.noreply.github.com>
2026-08-17 23:36:21 +0530

    Time: 391 ms (10.2%), Space: 42.7 MB (87.69%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,69 @@
+class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+
+                // Find an empty cell
+                if (board[row][col] == '.') {
+
+                    // Try numbers 1 to 9
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+
+                            // Place number
+                            board[row][col] = num;
+
+                            // Recursively solve remaining board
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Wrong choice → backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    // No number works
+                    return false;
+                }
+            }
+        }
+
+        // No empty cells left
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+
+        for (int i = 0; i < 9; i++) {
+
+            // Check row
+            if (board[row][i] == num) {
+                return false;
+            }
+
+            // Check column
+            if (board[i][col] == num) {
+                return false;
+            }
+
+            // Check 3x3 box
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+
+            if (board[boxRow][boxCol] == num) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+} 
